35c21591f23c12c3ee986c6db8b272eac97d8248
Eugene Kalenik <eugene.kalenik@instinctools.com>
2020-09-11 12:11:42 +0300

    reset page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-import { iTasks } from './itransition-tasks.js';
-import { otherTasks } from './other-tasks.js';
-
+import {iTasks} from './itransition-tasks.js';
+import {otherTasks} from './other-tasks.js';
 
 (function () {
   iTasks.map((task, index) => {
@@ -10,14 +9,22 @@ import { otherTasks } from './other-tasks.js';
         <td class="w-25">${task.title}</td>
         <td class="text-muted w-50">${task.description}</td>
         <td class="text-center">
-          ${task.demoPath === '' ? '-' : `<a href=${task.demoPath}>
+          ${
+            task.demoPath === ''
+              ? '-'
+              : `<a href=${task.demoPath}>
             <i class="fas fa-desktop"></i>
-          </a>`}
+          </a>`
+          }
         </td>
         <td class="text-center">
-          ${task.codePath === '' ? '-' : `<a href=${task.codePath}>
+          ${
+            task.codePath === ''
+              ? '-'
+              : `<a href=${task.codePath}>
             <i class="fas fa-code"></i>
-          </a>`}
+          </a>`
+          }
         </td>
       </tr>
     `;
@@ -30,16 +37,24 @@ import { otherTasks } from './other-tasks.js';
         <td class="w-25">${task.title}</td>
         <td class="text-muted w-50">${task.description}</td>
         <td class="text-center">
-          ${task.demoPath === '' ? '-' : `<a href=${task.demoPath}>
+          ${
+            task.demoPath === ''
+              ? '-'
+              : `<a href=${task.demoPath}>
             <i class="fas fa-desktop"></i>
-          </a>`}
+          </a>`
+          }
         </td>
         <td class="text-center">
-          ${task.codePath === '' ? '-' : `<a href=${task.codePath}>
+          ${
+            task.codePath === ''
+              ? '-'
+              : `<a href=${task.codePath}>
             <i class="fas fa-code"></i>
-          </a>`}
+          </a>`
+          }
         </td>
       </tr>
     `;
   });
-}());
+})();
